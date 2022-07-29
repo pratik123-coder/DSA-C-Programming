fd5d675fece807fdcee5353522f5160e215ae5df
pratik_codes <pratikmohanty939@yahoo.com>
2022-07-29 09:54:49 +0530

    Day ) Questions

---
+++ Day0_Q3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+void generateSubsequences(char str[], int n)
+{
+    int num = 1;
+    while (num <= pow(2, n) - 1)
+    {
+        int temp = num, i = 0;
+        while (i < n)
+        {
+            if ((temp >> i) & 1)
+                printf("%c", str[i]);
+            i++;
+        }
+        printf("\n");
+        num++;
+    }
+}
+
+int main()
+{
+    char str[100];
+    printf("Enter the string: ");
+    scanf("%s", &str);
+    int n = 0;
+    while (str[n] != '\0')
+        n++;
+    generateSubsequences(str, n);
+    return 0;
+}
